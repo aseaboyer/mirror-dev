8e19eb2dd520953cecdd7be5cf29458f2c599d6d
aseaboyer <aseaboyer@gmail.com>
2017-04-20 21:57:11 -0700

    Added icons, started api call

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-var dayEle, dateEle, timeEle, hourEle, minuteEle, secondEle, ampmEle,
+var und,
+    dayEle, dateEle, hourEle, minuteEle, secondEle, ampmEle, weatherIcon,
     days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function startTime() {
+function updateTime () {
     var today = new Date;
     var ampm = today.getHours () >= 12 ? 'pm' : 'am';
     
@@ -12,27 +13,40 @@ function startTime() {
     minuteEle.innerHTML = doubleDigits (today.getMinutes ());
     secondEle.innerHTML = doubleDigits (today.getSeconds ());
     ampmEle.innerHTML = ampm;
-    /*
-    timeEle.innerHTML = (today.getHours () % 12) + ":" +
-        today.getMinutes () + ":" + today.getSeconds () + 
-        ampm;
-    */
-    /*
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    var ampm = "am";
-    if (h >= 12) {
-        h -= 12;
-        ampm = "pm";
-    }
-    m = checkTime(m);
-    s = checkTime(s);
-    timeEle = h + ":" + m + ":" + s + " " + ampm;
-    */
-    var t = setTimeout(startTime, 500);
+
+    var t = setTimeout(updateTime, 500); // update time
 }
+
+function requestWeather () {
+    var dataBack,
+        updateFreq = (2 * 60 * 60 * 1000), // update in two hours
+        apiURL = "api.openweathermap.org/data/2.5/weather" + 
+            "?zip=98052,us&appid=eb2878d64c328aa9b2009211c2eb1271",
+        apiReq = new XMLHttpRequest();
+    
+    // ping the api
+    apiReq.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           updateWeather (xhttp.responseText);
+        }
+    };
+    apiReq.open("GET", apiURL, true);
+    apiReq.send();
+    
+    var t = setTimeout(requestWeather, updateFreq); // update every two hours
+}
+function updateWeather (data) {
+    var d = JSON.parse (data);
+    console.log (data);
+    
+    if (dataBack !== und) {
+        //weatherIcon.setAttribute ("class", "wi"); // set to just wi
+        // Add the specific class
+        //weatherIcon.classList.add ();
+    }
+    
+}
+
 function doubleDigits(i) {
     if (i < 10) {i = "0" + i}; // add zero in front of numbers < 10
     return i;
@@ -47,6 +61,12 @@ window.onload = function () {
     minuteEle = document.getElementById('current-minute');
     secondEle = document.getElementById('current-seconds');
     ampmEle = document.getElementById('current-ampm');
+    weatherIcon = document.getElementById('current-weather-icon');
     
-    startTime ();
+    updateTime ();
+    requestWeather ();
+    
+    console.log ("Bring in live weather");
+    console.log ("Transition alternative positoins with css, alter class with % something");
+    console.log ("Probably pump up those font sizes on the time");
 }
