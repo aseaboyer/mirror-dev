3ae8d119f8b86f777a0469692608656b9bca718c
aseaboyer <aseaboyer@gmail.com>
2017-04-21 20:52:35 -0700

    Add icons and temp

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var und,
-    dayEle, dateEle, hourEle, minuteEle, secondEle, ampmEle, weatherIcon, weatherText,
+    dayEle, dateEle, hourEle, minuteEle, secondEle, ampmEle, 
+    weatherIcon, weatherText, tempEle, 
     days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function updateTime () {
@@ -20,7 +21,7 @@ function updateTime () {
 function requestWeather () {
     var dataBack,
         updateFreq = (2 * 60 * 60 * 1000), // update in two hours
-        apiURL = "http://api.openweathermap.org/data/2.5/weather" + 
+        apiURL = "http://api.openweathermap.org/data/2.5/weather&units=metric" + 
             "?zip=98052,us&appid=eb2878d64c328aa9b2009211c2eb1271",
         apiReq = new XMLHttpRequest();
     
@@ -44,13 +45,43 @@ function updateWeather (data) {
     
     
     if (weatherID !== und) {
-        //weatherIcon.setAttribute ("class", "wi"); // set to just wi
+        weatherIcon.setAttribute ("class", "wi"); // clear old
         // Add a specific class
-        //weatherIcon.classList.add ();
+        if (weatherID >= 200 && weatherID <= 232) {
+            weatherIcon.classList.add ("wi-thunderstorm"); // thunder
+        } else if (weatherID >= 300 && weatherID <= 321) {
+            weatherIcon.classList.add ("wi-raindrops"); // drizzle
+        } else if (weatherID >= 500 && weatherID <= 531) {
+            weatherIcon.classList.add ("wi-rain"); // rain
+        } else if (weatherID >= 600 && weatherID <= 622) {
+            weatherIcon.classList.add ("wi-snow"); // snow
+        } else if (weatherID >= 700 && weatherID <= 781) {
+            weatherIcon.classList.add ("wi-fog"); // mist/fog
+        } else if (weatherID == 800) {
+            weatherIcon.classList.add ("wi-day-sunny"); // clear
+        } else if (weatherID >= 801 && weatherID <= 804) {
+            weatherIcon.classList.add ("wi-cloud"); // clouds
+        } else if (weatherID >= 900 && weatherID <= 906) {
+            weatherIcon.classList.add ("wi-storm-showers"); // serious weather
+        } else if (weatherID >= 951 && weatherID <= 955) {
+            weatherIcon.classList.add ("wi-windy"); // breeze
+        } else if (weatherID >= 956 && weatherID <= 957) {
+            weatherIcon.classList.add ("wi-strong-wind"); // strong wind
+        } else if (weatherID >= 958 && weatherID <= 962) {
+            weatherIcon.classList.add ("wi-storm-showers"); // strong storm
+        }
         
+        // set the text
         weatherText.innerHTML = textVal;
+        
+        // set the temp
+        tempEle.innerHTML = d.main.temp + "&#176;";
     }
     
+}
+
+function kelvinToC (k) {
+    return k - 273.15;
 }
 
 function doubleDigits(i) {
@@ -69,6 +100,7 @@ window.onload = function () {
     ampmEle = document.getElementById('current-ampm');
     weatherIcon = document.getElementById('current-weather-icon');
     weatherText = document.getElementById('current-weather-text');
+    tempEle = document.getElementById('current-temp');
     
     updateTime ();
     requestWeather ();
